f8d63486187af4b4f9bf2979fa9177bc49e743a7
崔 明辉 <cuis@vip.qq.com>
2021-05-31 21:41:26 +0800

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
-main(List<String> args) {
+import 'dart:convert';
+import 'dart:io';
+
+main(List<String> args) async {
+  final server = await HttpServer.bind('0.0.0.0', 9898);
+  await for (var req in server) {
+    req.response.add(utf8.encode(
+        '<html><body style="background-color: white">Testing...</body></html>'));
+    req.response.close();
+  }
   print("Testing...");
 }
